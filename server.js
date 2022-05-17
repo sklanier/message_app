d342371211ad3872db4d906df8c988f4d18279b2
sklanier <stevekentlanier@gmail.com>
2022-05-17 10:18:17 -0500

    added users array as hard data

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 import {ApolloServer, gql} from 'apollo-server';
 
+const users = [
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: "john@doe.com",
+        password: "123456"
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: "jane@doe.com",
+        password: "123457"
+    },
+];
+
 const typeDefs = gql`
     type Query {
         greet: String
